48e2164582780961fcd5f64ea0e093f319dde11e
eric <cgx1997@outlook.com>
2022-04-30 16:59:07 -0700

    updated training scripts

--- experiment_scripts/train_2d_albert.sh
+++ experiment_scripts/train_2d_albert.sh
@@ -1,1 +1,1 @@
-python experiment_scripts/train_img.py --experiment_name albert --model_type=sine --image_path data/albert_gt.jpg --num_epochs 10000 --point_batch_size 1500000 --steps_til_summary 5
+python experiment_scripts/train_img.py --experiment_name albert --model_type=sine --image_path data/albert_gt.jpg --num_epochs 50000 --point_batch_size 2097152 --epochs_til_ckpt 250 --steps_til_summary 100
