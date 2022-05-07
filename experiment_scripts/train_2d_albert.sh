c72fbc0bf331ec03348367a229df6f879ee448b3
eric <cgx1997@outlook.com>
2022-05-06 21:38:22 -0700

    increased num of hidden layers and reduced batch sizes accordingly

--- experiment_scripts/train_2d_albert.sh
+++ experiment_scripts/train_2d_albert.sh
@@ -1,1 +1,1 @@
-python experiment_scripts/train_img.py --experiment_name albert --model_type=sine --image_path data/albert_gt.jpg --num_epochs 50000 --point_batch_size 1048576 --epochs_til_ckpt 1 --steps_til_summary 1000
+python experiment_scripts/train_img.py --experiment_name albert --model_type=sine --image_path data/albert_gt.jpg --num_epochs 50000 --point_batch_size 524288 --eval_patch_size 524288 --epochs_til_ckpt 1 --steps_til_summary 1000
