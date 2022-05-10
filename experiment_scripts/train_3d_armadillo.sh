2a21a17d5c4bcbdb392852513b7678de893284c7
eric <cgx1997@outlook.com>
2022-05-10 16:30:57 -0700

    added armadillo training script

---
+++ experiment_scripts/train_3d_armadillo.sh
@@ -0,0 +1,8 @@
+python experiment_scripts/train_sdf.py \
+--batch_size 4096 \
+--num_epochs 400 \
+--model_type sine \
+--point_cloud_path data/our_shapes/armadillo.xyz \
+--experiment_name armadillo \
+--epochs_til_ckpt 100 \
+--steps_til_summary 5000
