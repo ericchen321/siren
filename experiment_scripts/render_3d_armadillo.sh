61d3855f8ec0bf525daaec2b43fd3ead308aa17b
eric <cgx1997@outlook.com>
2022-05-10 17:30:29 -0700

    created rendering script for armadillo

---
+++ experiment_scripts/render_3d_armadillo.sh
@@ -0,0 +1,5 @@
+python experiment_scripts/test_sdf.py \
+--checkpoint_path /home/eric/cpsc533r/project/siren/logs/armadillo/checkpoints/model_current.pth \
+--batch_size 8192 \
+--resolution 512 \
+--experiment_name armadillo
