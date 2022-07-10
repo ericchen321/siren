b60bd95f8a95c265b61fad2bc12dc10d67865397
typan <sampty@student.ubc.ca>
2022-07-09 17:03:12 -0700

    add siren train_3d scripts

--- experiment_scripts/cc_scripts/sdf/default/train_3d_lunar_lander_cc.sh
+++ experiment_scripts/cc_scripts/sdf/default/train_3d_lunar_lander_cc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=02:00:00
 #SBATCH --account=def-rhodin
-#SBATCH --job-name=tr_thai_statue_default_siren
+#SBATCH --job-name=tr_lunar_lander_default_siren
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --mem=24G
 module load python/3.6
@@ -11,4 +11,4 @@ module load cuda/11.0
 cd /home/gxc321/
 source SirenEnv/bin/activate
 cd /home/gxc321/scratch/siren/
-source experiment_scripts/train_3d.sh thai_statue default 200000 4096 10000 5000
+source experiment_scripts/train_3d.sh lunar_lander default 200000 4096 10000 5000
